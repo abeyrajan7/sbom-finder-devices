6e261436fbe7de347c5cc706beb17c6fa8714a15
Abey K Rajan <abeyrajan7@gmail.com>
2025-03-07 14:20:20 -0500

    Initial commit - SBOM Finder backend setup

---
+++ src/main/java/com/sbomfinder/SbomFinderApplication.java
@@ -0,0 +1,11 @@
+package com.sbomfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbomFinderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SbomFinderApplication.class, args);
+    }
+}
